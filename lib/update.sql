ce38feb9113126dcc8bcdd928b02020371125230
krissasantos <krissa.santos31@gmail.com>
2016-10-07 11:04:27 -0400

    Done.

--- lib/update.sql
+++ lib/update.sql
@@ -1,1 +1,4 @@
-UPDATE characters SET species = "Martian" WHERE id = 8;
+-- UPDATE characters SET species = "Martian" WHERE id = 8; hardcode
+
+UPDATE characters SET species = "Martian" ORDER BY id DESC LIMIT 1;
+
